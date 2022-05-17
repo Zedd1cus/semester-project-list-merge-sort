b21b86dd0965691ce9c9ded45b719073fbb9aa21
ninza <ninza147258@gmail.com>
2022-05-17 09:27:51 +0300

    benchmark done

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -4,19 +4,32 @@ from typing import List
 import time
 
 
-def fill_dynamic_array(new_array: List[str], array_from_data: List[str]) -> List[str]:
-    pass
+def fill_dynamic_array(new_array: List[int], array_from_data: List[int]) -> List[int]:
+    new_array = array_from_data
+    return new_array
 
-
-def fill_linked_list(new_array: List[str], array_from_data: List[str]) -> List[str]:
-    pass
-
+def fill_linked_list(linklist: LinkedList, array_from_data: List[int]) -> LinkedList:
+    for elem in array_from_data:
+        linklist.append(elem)
+    return linklist
 
 def bench_dynamic_array(path: str) -> float:
-    pass
+    dyn_array = []
+    with open(path) as data:
+        arr = [int(line) for line in data.readlines()]
+        fill_dynamic_array(dyn_array, arr)
+    time_lst = time.time()
+    dyn_array = dynamic_array_merge_sort(dyn_array)
+    return (time.time() - time_lst)*1000
 
 
 def bench_linked_list(path: str) -> float:
-    pass
+    linklist = LinkedList()
+    with open(path) as data:
+        arr = [int(line) for line in data.readlines()]
+        fill_linked_list(linklist, arr)
+    time_lst = time.time()
+    linklist.merge_sort(linklist.head)
+    return (time.time() - time_lst)*1000
 
 
