4f1578be76507f2d4937241eb9d41ea0db6edbc5
ninza <ninza147258@gmail.com>
2022-05-21 12:23:14 +0300

    graphs done

--- dataset/graphs.py
+++ dataset/graphs.py
@@ -1,50 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from generate_txt_dataset import arr_for_elements
 
 
 def array_plot() -> None:
     fig, ax = plt.subplots()
 
-    x = [100, 500, 900, 1300, 1700, 2100, 2500, 2900]
-    y = [9.49, 167.11, 385.48, 829.21, 22600.04, 1631.36, 2838.84, 2603.60]
+    x = arr_for_elements
+    y = [0.31235218, 26.93439484, 56.89840794, 87.54626274, 120.563755, 163.9518452]
 
-    x1 = [100, 2900]
-    y1 = [9.49, 2603.60]
+    x1 = arr_for_elements
+    y1 = [0.312495232, 21.712327, 46.7825985, 75.17297745, 101.3124561, 141.7034006]
 
-    ax.plot(x1, y1, label='предполагалось')
-    ax.plot(x, y, label='получилось')
-
-    ax.plot()
-
-    ax.legend(loc='upper left')
-
-    ax.set_title('Dynamic_Array')
-    ax.set_ylabel('Время работы(мс)')
-    ax.set_xlabel('Кол-во элементов')
-
-    plt.show()
-
-
-def linklist_plt() -> None:
-    fig, ax = plt.subplots()
-
-    x = [100, 500, 900, 1300, 1700, 2100, 2500, 2900]
-    y = [1.60, 7.52, 8.26, 17.53, 12.91, 16.40, 28.90, 24.70]
-
-    one = 9.49 / 1000
-
-    z = x[:]
-    z.append(1)
-    z.sort()
-
-    ax.plot(x, y, label='получилось')
-    ax.plot(z, 0, label='предполагалось')
+    one_oper = y1[3] / (15100*14)
+    x2 = arr_for_elements
+    list_cnt_operations = [700, 5100*13, 10100*14, 15100*14, 20100*15, 25100*15]
+    y2 = [one_oper*i for i in list_cnt_operations]
+    ax.plot(x1, y1, label='link list', color='r')
+    ax.plot(x, y, label='dynamic array', color='b')
+    ax.plot(x2, y2, label='Предполагалось', color='k')
 
     ax.plot()
 
     ax.legend(loc='upper left')
 
-    ax.set_title('Linked_List')
+    ax.set_title('Merge Sort')
     ax.set_ylabel('Время работы(мс)')
     ax.set_xlabel('Кол-во элементов')
 
@@ -52,4 +32,3 @@ def linklist_plt() -> None:
 
 if __name__ == '__main__':
     array_plot()
-    linklist_plt()
